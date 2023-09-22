6c3fb27f41f84328e2fe5e53de93338e58254f43
kaarlinks <adeotiade@gmail.com>
2023-09-22 21:55:16 +0100

    The first git commit

---
+++ src/test/java/com/kaarlinks/introtogit/IntrotogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaarlinks.introtogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrotogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
